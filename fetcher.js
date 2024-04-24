24d1daabcfc9960565e05658a2343c76506f04dc
Saurlax <saurlax@qq.com>
2024-04-24 12:07:40 +0800

    fix: use sync to fetch

--- fetcher.js
+++ fetcher.js
@@ -9,12 +9,6 @@ const targets = [
     link: a => a.href.replace('..', 'https://ss.dlut.edu.cn')
   },
   {
-    url: 'https://eda.dlut.edu.cn/tzgg/tzgg.htm',
-    selector: '.ny_list ul li',
-    title: a => a.textContent,
-    link: a => a.href.replace('..', 'https://eda.dlut.edu.cn')
-  },
-  {
     url: 'https://teach.dlut.edu.cn/byxx/byxx.htm',
     selector: '.list ul li',
     title: a => a.title,
@@ -22,16 +16,18 @@ const targets = [
   }
 ]
 
-export async function update () {
-  const tasks = targets.map(async target => {
+export async function update() {
+  let results = []
+  for (const target of targets) {
     const res = await fetch(target.url)
     const doc = new JSDOM(await res.text()).window.document
-    return Array.from(doc.querySelectorAll(target.selector))
+    const items = Array.from(doc.querySelectorAll(target.selector))
       .slice(0, 5)
       .map(li => li.querySelector('a'))
       .map(a => {
         return { title: target.title(a), link: target.link(a) }
       })
-  })
-  return (await Promise.all(tasks)).flat()
+    results.push(...items)
+  }
+  return results
 }
