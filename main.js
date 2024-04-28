0b463ca90d444672d3e957f317a0f6b440af32dd
Saurlax <saurlax@qq.com>
2024-04-28 22:01:22 +0800

    feat: stop sending annoying error

--- main.js
+++ main.js
@@ -2,14 +2,6 @@ import { send } from './sender.js'
 import { update } from './fetcher.js'
 import { getData, setData } from './db.js'
 
-process.on('unhandledRejection', (reason, p) => {
-  send(reason.stack)
-})
-
-process.on('uncaughtException', err => {
-  send(err.stack)
-})
-
 const data = await update()
 const old = getData()
 data
